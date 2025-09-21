48f0a1a1609ba9f2ca3600689c672c807f8d0d5e
ArthurFilipe981 <arthurfilipevitorianofaria@gmail.com>
2025-09-21 14:42:23 -0300

    aprendendo docker com spring

--- Aula-de-Docker/src/main/java/Docker/Aula/de/Docker/Controller.java
+++ Aula-de-Docker/src/main/java/Docker/Aula/de/Docker/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class Controller {
 
-
+    //so para testar o docker
     @GetMapping("/docker")
     public String mensagen(){
         return "Essa é uma aplicaçao Docker";
